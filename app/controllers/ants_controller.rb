4c63e4377007fe5a5cc77b7ce74332a444844b57
Juanset <jnset@hotmail.com>
2017-04-16 12:51:53 -0500

    Ants impplemented ok
    Add Json to convert string to array

--- app/controllers/ants_controller.rb
+++ app/controllers/ants_controller.rb
@@ -1,4 +1,5 @@
 class AntsController < ApplicationController
+  require "json"
   before_action :set_ant, only: [:show, :edit, :update, :destroy]
 
   # GET /ants
@@ -164,7 +165,8 @@ class AntsController < ApplicationController
 
 def algorithm()
   @ant = Ant.find(params[:id])
-  @berlin52 = @ant.set.scan(/\d/).map(&:to_i)
+  @berlin52 =  JSON.parse(@ant.set)
+  #@berlin52 = @ant.set.scan(/\d/).map(&:to_i)
   #@berlin52= [[565,575],[25,185],[345,750],[945,685],[845,655],[880,660],[25,230],[525,1000],[580,1175],[650,1130],[1605,620],[1220,580],[1465,200],[1530,5],[845,680],[725,370],[145,665], [415,635],[510,875],[560,365],[300,465],[520,585],[480,415], [835,625],[975,580],[1215,245],[1320,315],[1250,400],[660,180],[410,250],[420,555],[575,665],[1150,1160],[700,580],[685,595],[685,610],[770,610],[795,645],[720,635],[760,650],[475,960],              [95,260],[875,920],[700,500],[555,815],[830,485],[1170,65],[830,610],[605,625],[595,360],[1340,725],[1740,245]]
   @best = search(@berlin52, @ant.max_it, @ant.num_ants, @ant.decay_factor.to_f, @ant.c_heur.to_f, @ant.c_hist.to_f)
   puts "Done. Best Solution: c=#{@best[:cost]}, v=#{@best[:vector].inspect}"
